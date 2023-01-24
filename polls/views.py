ab96517343d1771eae54ec32dd421b283f84b287
Anamika sinha <61617222+AnamikaSinha2020@users.noreply.github.com>
2023-01-23 22:14:40 -0500

    views updated

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,15 @@ def index(request):
     return HttpResponse("<http>Hello World!! you are at poll index.</http>")
 
 # Create your views here.
+def details(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,question_id):
+    response = "You're looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voteing on question %s." %question_id)
+
+
+
